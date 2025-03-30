5d52adb156db907c4d777453e2a2a5a53e825bf6
Salah Omar Moussa <salahmoussa2222@gmail.com>
2025-03-30 23:04:31 +0300

    Import Fix entity and module

--- rest/src/dashboard/dashboard.module.ts
+++ rest/src/dashboard/dashboard.module.ts
@@ -8,13 +8,16 @@ import { Complaint } from '../complaint/complaint.entity';
 import { UserModule } from '../user/user.module';
 import { VehicleModule } from '../vehicle/vehicle.module';
 import { ComplaintModule } from '../complaint/complaint.module';
+import { FixModule } from 'src/fix/fix.module';
+import { Fix } from 'src/fix/fix.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, Vehicle, Complaint]), 
+    TypeOrmModule.forFeature([User, Vehicle, Complaint, Fix]), 
     UserModule, 
     VehicleModule, 
-    ComplaintModule
+    ComplaintModule,
+    FixModule
   ],
   controllers: [DashboardController],
   providers: [DashboardService],
