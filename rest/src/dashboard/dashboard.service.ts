556de22b64db9fab5c75622fc97bec52119217d8
Salah Omar Moussa <salahmoussa2222@gmail.com>
2025-03-30 23:04:47 +0300

    Add mechanic dashboard stats

--- rest/src/dashboard/dashboard.service.ts
+++ rest/src/dashboard/dashboard.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Complaint } from 'src/complaint/complaint.entity';
+import { Fix } from 'src/fix/fix.entity';
 import { User } from 'src/user/user.entity';
 import { Vehicle } from 'src/vehicle/vehicle.entity';
 import { Repository } from 'typeorm';
@@ -12,6 +13,7 @@ export class DashboardService {
     @InjectRepository(User) private userRepo: Repository<User>,
     @InjectRepository(Vehicle) private vehicleRepo: Repository<Vehicle>,
     @InjectRepository(Complaint) private complaintRepo: Repository<Complaint>,
+    @InjectRepository(Fix) private fixRepo: Repository<Fix>,
   ) {}
 
   async getAdminDashboardStats() {
@@ -22,5 +24,12 @@ export class DashboardService {
     const totalComplaints = await this.complaintRepo.count();
 
     return { totalUsers, totalMechanics, totalAdmins, totalVehicles, totalComplaints };
+  }
+
+  async getMechanicDashboardStats() {
+    const totalComplaints = await this.complaintRepo.count();
+    const totalFixes = await this.fixRepo.count();
+
+    return { totalComplaints, totalFixes };
   }
 }
