bc2e04d20435baa050bd7b5f238c936097508edd
Salah Omar Moussa <salahmoussa2222@gmail.com>
2025-04-05 15:13:51 +0300

    Update Home.jsx to display car brands

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,16 +1,39 @@
 import Menu from "../components/Menu";
 import Footer from "../components/Footer";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
 const Home = () => {
+  const [carMakes, setCarMakes] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/vehicles/makes")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Fetched car makes:", data);
+        setCarMakes(data);
+      })
+      .catch((err) => console.error("Error fetching car makes:", err));
+  }, []);
+  
+
     return (
       <>
         <Menu />
-        <div className="holderhome">
-          <h2 style={{color: 'white'}}>Select your Vehicle</h2>
-          <div className="makeholders">
-            
-          </div>
-        </div>
-        <Footer />
+      <div className="holderhome">
+        <h2 style={{ color: "white" }}>Select your Vehicle</h2>
+        <ul className="carmakes">
+          {carMakes.map((make, index) => (
+            <li key={index}>
+              <Link to={`/cars/${make.toLowerCase()}`}>
+                <img src={`/${make.toLowerCase()}.png`} alt={make} />
+                <h2>{make}</h2>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <Footer />
       </>
       );
 }
