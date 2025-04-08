913a5807b81150d5115b810da44cfaf544eb19bc
Salah Omar Moussa <salahmoussa2222@gmail.com>
2025-04-08 17:54:35 +0300

    Update main.ts to support file upload

--- rest/src/main.ts
+++ rest/src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
     origin: 'http://localhost:5173', // Change to match your frontend URL
     credentials: true,
+  });
+
+  app.useStaticAssets(join(__dirname, '..', 'public'), {
+    prefix: '/',
   });
 
   await app.listen(3000);
