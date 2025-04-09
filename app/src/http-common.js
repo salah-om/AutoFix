df3de40ca625f9d31dd39b0f4163db0833397b50
Salah Omar Moussa <salahmoussa2222@gmail.com>
2025-04-09 15:35:36 +0300

    Update http-common.js

--- app/src/http-common.js
+++ app/src/http-common.js
@@ -1,8 +1,22 @@
 import axios from "axios";
 
-export default axios.create({
-    baseURL: "http://localhost:3000",
-    headers: {
-        "Content-Type": "application/json"
-    }
+const http = axios.create({
+  baseURL: "http://localhost:3000",
+  headers: {
+    "Content-Type": "application/json"
+  }
 });
+
+// Attach token from localStorage as Bearer token
+http.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default http;
