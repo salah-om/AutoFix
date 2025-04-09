0ecbcf78d0298884778b8d5e9d7bdf4460ffda5e
Salah Omar Moussa <salahmoussa2222@gmail.com>
2025-04-09 15:30:58 +0300

    Update jwt.strategy.ts

--- rest/src/auth/jwt.strategy.ts
+++ rest/src/auth/jwt.strategy.ts
@@ -14,6 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return { userId: payload.sub, username: payload.username };
+    console.log('JWT payload:', payload);
+    return { id: payload.sub, username: payload.username };
   }
 }
