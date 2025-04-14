6aa7ba6ead41a4f569ff93e41c8f92f5f4d10ae2
Salah Omar Moussa <salahmoussa2222@gmail.com>
2025-04-14 19:32:08 +0300

    Create SearchComplaints.jsx

---
+++ app/src/components/SearchComplaints.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
+import http from "../http-common";
+import Menu from "./Menu";
+import Footer from "./Footer";
+import ComplaintList from "./ComplaintList";
+
+const SearchComplaints = () => {
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get('q');
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchResults = async () => {
+      try {
+        setLoading(true);
+        const response = await http.get(`/complaints/search?query=${query}`);
+        setResults(response.data);
+      } catch (error) {
+        console.error("Search error:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (query) fetchResults();
+  }, [query]);
+
+  return (
+    <>
+      <Menu />
+      <div className="container mt-4">
+        {loading ? (
+          <div className="text-center my-5">
+            <div className="spinner-border text-warning" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        ) : (
+          <ComplaintList complaints={results} title={`Search Results for "${query}"`}/>
+        )}
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default SearchComplaints;
